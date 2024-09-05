bb072964944225be922b757cb3c908b90a5a9844
Shashwat200419 <154683376+Shashwat200419@users.noreply.github.com>
2024-09-05 23:12:34 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const hamburgerButton = document.getElementById('menu-btn');
+const mobileMenu = document.getElementById('menu');
+
+
+hamburgerButton.addEventListener('click', ()=>{
+    hamburgerButton.classList.toggle('open');
+    mobileMenu.classList.toggle('flex');
+    mobileMenu.classList.toggle('hidden');
+})
+
+
